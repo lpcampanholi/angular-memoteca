b339e7c8a61634bfe7620f1f04576cceb2cd1759
lp <lpcampanholi@gmail.com>
2025-01-13 18:08:16 -0300

    isso aí

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './components/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './components/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listar',
+    pathMatch: 'full'
+  },
+  {
+    path: 'criar',
+    component: CriarPensamentoComponent
+  },
+  {
+    path: 'listar',
+    component: ListarPensamentoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
